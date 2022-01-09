aa86a2b487365c95324c9ceead4ceb16bd8ebcd7
Jarosław Jusiak <jaroslaw.jusiak@gmail.com>
2022-01-09 18:48:36 +0100

    Dodanie aplikacji angularowej. Wyczyszczenie app.html Dodanie projektu firebase. Przetestowany deploy na https://splendor-5f7e5.firebaseapp.com

---
+++ splendor-app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'splendor-5f7e5',
+    appId: '1:1076169752814:web:5d76aecc85328b605a73ff',
+    storageBucket: 'splendor-5f7e5.appspot.com',
+    apiKey: 'AIzaSyBJtb1R4rRylQU8_ze21203_DygUtCU-nA',
+    authDomain: 'splendor-5f7e5.firebaseapp.com',
+    messagingSenderId: '1076169752814',
+  },
+  production: true
+};
---
+++ splendor-app/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'splendor-5f7e5',
+    appId: '1:1076169752814:web:5d76aecc85328b605a73ff',
+    storageBucket: 'splendor-5f7e5.appspot.com',
+    apiKey: 'AIzaSyBJtb1R4rRylQU8_ze21203_DygUtCU-nA',
+    authDomain: 'splendor-5f7e5.firebaseapp.com',
+    messagingSenderId: '1076169752814',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
